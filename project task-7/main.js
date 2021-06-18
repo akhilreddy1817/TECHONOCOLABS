0f3d9215738e87c7a92f6847fca0261d5443fcee
akhilreddy1817 <85801472+akhilreddy1817@users.noreply.github.com>
2021-06-18 16:34:21 +0530

    Create main.js

---
+++ project task-7/main.js
@@ -0,0 +1,16 @@
+var myage = 20;
+var earlyYears = 2;
+// created two variables and assigned values to it.
+
+let earlyYears1 = earlyYears * 10.5;
+laterYears = myage - 2;
+laterYears = laterYears * 4;
+// calculating laterYears by 4 .
+
+myAgeInDogYears = earlyYears1 + laterYears;
+var name = 'AKHIL';
+let myName = name.toLowerCase();
+// initializing a string and change it to lowercase using .toLowercase() and stored it in myName.
+
+console.log(`My name is ${myName}.I am ${myage}years old human years which is${myAgeInDogYears}years old in dog years.`)
+
