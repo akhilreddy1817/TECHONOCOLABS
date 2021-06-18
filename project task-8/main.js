2aa0b6b175b3c298c20d11c775616a858e844320
akhilreddy1817 <85801472+akhilreddy1817@users.noreply.github.com>
2021-06-18 16:36:15 +0530

    Create main.js

---
+++ project task-8/main.js
@@ -0,0 +1,39 @@
+
+var userName =' ';
+'jane'?console.log(`Hello, Jane`) :
+console.log(`Hello`);
+
+var userQuestion = 'am i akhil?';
+console.log(`am i akhil?`);
+
+var randomNumber = Math.floor(Math.random() * 8);
+var eightBall = ' ';
+
+switch(eightBall)
+{
+    case 0:
+        eightBall = 'it is certain'
+        break;
+    case 1:
+         eightBall = 'it is decidedly so'
+         break;
+     case 2:
+         eightBall = 'Reply hazy try again'
+         break;
+    case 3:
+        eightBall = 'Cannot predict now'
+        break;
+    case 4:
+         eightBall = 'Do not count on it'
+         break; 
+     case 5:
+         eightBall = 'My sources say no'
+         break;  
+     case 6:
+        eightBall = 'Outlook not so good'
+        break; 
+    case 7:
+        eightBall = 'Signs point to yes'
+        break;              
+}
+console.log(`the eight ball answers: ${eightBall}`)
